0317488ea0658e8adaa738c83a99a0734f6d8049
François P <francois.plessier@gmail.com>
2023-11-30 08:58:12 +0100

    Avoiding error msgs

--- mixxx_prepare.sh
+++ mixxx_prepare.sh
@@ -3,25 +3,21 @@
 # we use the recommendantions from this page:
 # https://github.com/mixxxdj/mixxx/wiki/Adjusting-Audio-Latency
 
-
 # the commands needs to be called with sudo
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 # CPU frequency scaling is a main cause of Mixxx skipping on laptops.
-echo "Disabling CPU frequency scaling"
 for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
-	echo performance > $i
+	cpunum=$(echo $i | grep -Po "\d+")
+	msg="Disabling CPU frequency scaling for CPU #$cpunum"
+	test "$(cat $i)" = "performance" || echo $msg && echo "performance" > $i
 done
 
 # Simultaneous Multithreading (SMT), or HyperThreading (HT) as Intel calls it,
 # can make some programs faster but makes realtime audio software
 # like Mixxx much more likely to glitch.
-echo "Disabling hyperthreading"
-echo off > /sys/devices/system/cpu/smt/control
-
-
-# ATTENTION: this is an extra for my personnal use
-# I use GenyMotion which does not stops Android Debug Bridge when it's closed
-pgrep adb && echo "Killing Android Debug Bridge" && killall adb
+i=/sys/devices/system/cpu/smt/control
+msg="Disabling hyperthreading" 
+test "$(cat $i)" = "on" && echo $msg && echo "off" > $i
 
 
