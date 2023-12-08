325383886f306bac3ddb81b73f7dd7437ea31f1e
François P <francois.plessier@gmail.com>
2023-12-08 14:09:29 +0100

    old config removed

--- python_tools/utils/user_parameters.py
+++ python_tools/utils/user_parameters.py
@@ -1,16 +1,6 @@
 MIXXX_DB = "${HOME}/.mixxx/mixxxdb.sqlite"
 MIXXX_FOLDER = "${HOME}/Mixxx/"
 
-# parameters for ../mixxx_to_rekordbox_xml/mixxx_to_rekordbox_xml.py
-# (Rekordbox export)
-REKBOX_OUTPUT_FILE = "rekordbox_output.xml"
-ORIGINAL_MOUNT_POINT = "/media/francois/MEGAMIX"
-FINAL_MOUNT_POINT = "E:/"
-# use a trick to calculate the "inizio" using the BPM and one of hot cues (assuming it is on a beat)
-GUESS_INIZIO = True
-# The hotcues will be converted to more robust memory cues. Do we keep the original hotcues after conversion?
-KEEP_HOT_CUES = False
-
 # parameters for ../fix_track_paths/clementine_custom_music_db.py
 # (Fixing tacks locations)
 # The names must correspond to the ones in fix_track_paths/mixxxdb_fix_tracks_locations.sql file
