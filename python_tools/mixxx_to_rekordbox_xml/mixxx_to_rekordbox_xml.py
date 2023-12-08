d19541d6428cfe43014888ae3cde5757abb86d75
François P <francois.plessier@gmail.com>
2023-12-08 14:06:01 +0100

    playlists fix and inizio guess, also ini file

--- python_tools/mixxx_to_rekordbox_xml/mixxx_to_rekordbox_xml.py
+++ python_tools/mixxx_to_rekordbox_xml/mixxx_to_rekordbox_xml.py
@@ -1,5 +1,6 @@
-from sys import path
-from typing import Optional
+from sys import path, exit
+import configparser
+from typing import Optional, Callable, Generator, Mapping
 from pathlib import Path
 from urllib.parse import quote
 from xml.etree import ElementTree as ET
@@ -17,17 +18,27 @@ from utils.music_db_utils import (
     open_mixxx_playlist_tracks,
 )
 
+AttribDict = Mapping[str, int | float | str]
 
-OUTPUT_FILE = "rekordbox_output.xml"
+
+config_file = Path(__file__).parent / Path("config.ini")
+config = configparser.ConfigParser()
+config.read(config_file)
+params = config["Default"]
+
+ans = print(f"The following parameters are defined in {config_file}")
+for k, v in params.items():
+    print(f"  {k}: {v}")
+ans = input("Are you OK with that (y/*)? ")
+if ans != "y":
+    exit()
 
 
-def stringify_dict(d: dict) -> dict[str:str]:
+def stringify_dict(d: AttribDict) -> dict[str, str]:
     return {k: str(v) for k, v in d.items()}
 
 
-def get_elem(
-    name: str, attrib: Optional[dict["str", int | float | str]] = None
-) -> ET.Element:
+def get_elem(name: str, attrib: Optional[AttribDict] = None) -> ET.Element:
     if attrib is None:
         attrib = {}
     return ET.Element(name, attrib=stringify_dict(attrib))
@@ -54,35 +65,66 @@ def get_node_xml(nb_playlists) -> ET.Element:
     return get_elem("NODE", attrib)
 
 
+def guess_inizio(bpm: float, some_hot_cue: float) -> float:
+    beat_length = 60.0 / bpm
+    inizio = some_hot_cue % beat_length
+    if inizio > beat_length / 2:
+        inizio -= beat_length
+    return inizio
+
+
+def get_tempo_xml(bpm: float, some_hot_cue: float) -> ET.Element:
+    attrib: AttribDict = {
+        "Inizio": guess_inizio(bpm, some_hot_cue),
+        "Bpm": bpm,
+        "Metro": "4/4",
+        "Battito": "1",
+    }
+    return get_elem("TEMPO", attrib)
+
+
 def mixxx_track_row_to_rekbox_xml(row: pd.Series) -> ET.Element:
-    attrib = {
+    location = row["location_y"]
+    final_location = location.replace(
+        params["original_mount_point"], params["final_mount_point"]
+    )
+    attrib: AttribDict = {
         "TrackID": row["id_x"],
         "Name": row["title"],
         "Artist": row["artist"],
         "Album": row["album"],
         "TrackNumber": row["tracknumber"],
         "Genre": row["genre"],
-        # Unit : Second (without decimal numbers)
         "TotalTime": round(row["duration"]),
         "Tonality": row["key"],
         "AverageBpm": row["bpm"],
-        "Location": quote(row["location_y"]),
+        "Location": quote("file://localhost/" + final_location),
         "SampleRate": row["samplerate"],
     }
     return get_elem("TRACK", attrib)
 
 
-def mixxx_cue_row_to_rekbox_xml(row: pd.Series, samplerate) -> ET.Element:
-    attrib = {
-        "Type": "0",
-        "Num": row["hotcue"],
-        "Start": row["position"] / float(samplerate) / 2,
-    }
-    return get_elem("POSITION_MARK", attrib)
+def mixxx_cue_to_rekordbox_cue(mixxx_position: float, samplerate: float) -> float:
+    return mixxx_position / samplerate / 2
+
+
+def mixxx_cue_row_to_rekbox_xml(
+    row: pd.Series, samplerate: float
+) -> Generator[ET.Element, None, None]:
+    cue_nums = [-1]
+    if params["keep_hot_cues"]:
+        cue_nums.append(row["hotcue"])
+    for cnum in cue_nums:
+        attrib: AttribDict = {
+            "Type": "0",
+            "Num": cnum,
+            "Start": mixxx_cue_to_rekordbox_cue(row["position"], samplerate),
+        }
+        yield get_elem("POSITION_MARK", attrib)
 
 
 def mixxx_playlist_to_rekordbox_xml(row: pd.Series, track_numbers: int) -> ET.Element:
-    attrib = {
+    attrib: AttribDict = {
         "Name": row["name"],
         "Type": "1",
         "KeyType": "0",
@@ -113,9 +155,21 @@ if __name__ == "__main__":
     for _, track_row in df_merge.iterrows():
         track_xml = mixxx_track_row_to_rekbox_xml(track_row)
         track_cues = mixxx_cues[mixxx_cues["track_id"] == track_row["id_x"]]
+        rate = track_xml.get("SampleRate")
         for _, cue_row in track_cues.iterrows():
-            cue_xml = mixxx_cue_row_to_rekbox_xml(cue_row, track_xml.get("SampleRate"))
-            track_xml.append(cue_xml)
+            assert rate
+            frate = float(rate)
+            for cue_xml in mixxx_cue_row_to_rekbox_xml(cue_row, frate):
+                track_xml.append(cue_xml)
+        # this is a trick to calculate the "inizio" using the BPM and one of hot cues (assuming it is on a beat)
+        if params["guess_inizio"] and len(track_cues) > 0:  # we need a hot cue...
+            bpm, some_hot_cue = track_xml.get("AverageBpm"), cue_xml.get("Start")
+            if bpm and some_hot_cue:
+                fbpm = float(bpm)
+                if fbpm > 0.0:  # ... and a bpm
+                    fsome_hot_cue = float(some_hot_cue)
+                    tempo_xml = get_tempo_xml(fbpm, fsome_hot_cue)
+                    track_xml.append(tempo_xml)
         collection_xml.append(track_xml)
 
     # playlists
@@ -126,7 +180,7 @@ if __name__ == "__main__":
     for _, pls_row in mixxx_playlists.iterrows():
         pls_tracks = mixxx_playlist_tracks[
             mixxx_playlist_tracks["playlist_id"] == pls_row["id"]
-        ]
+        ].sort_values("position")
         playlist_node_xml = mixxx_playlist_to_rekordbox_xml(pls_row, len(pls_tracks))
         for _, pls_track_row in pls_tracks.iterrows():
             playlist_node_xml.append(
@@ -143,8 +197,9 @@ if __name__ == "__main__":
     tree = ET.ElementTree(element=root_xml)
     ET.indent(tree, space="  ", level=0)
 
-    with open(OUTPUT_FILE, "w") as fxml:
+    rek_fil = params["rekordbox_xml_file"]
+    with open(rek_fil, "w") as fxml:
         fxml.write('<?xml version="1.0" encoding="UTF-8"?>')
-    tree.write(OUTPUT_FILE, encoding="unicode")
+    tree.write(rek_fil, encoding="unicode")
 
-    print(f"==> {OUTPUT_FILE}")
+    print(f"==> {rek_fil}")
--- python_tools/utils/user_parameters.py
+++ python_tools/utils/user_parameters.py
@@ -1,11 +1,26 @@
 MIXXX_DB = "${HOME}/.mixxx/mixxxdb.sqlite"
 MIXXX_FOLDER = "${HOME}/Mixxx/"
-CLEM_DB = "${HOME}/.config/Clementine/clementine.db"
 
-# The names must correspond to the ones in the SQL file
+# parameters for ../mixxx_to_rekordbox_xml/mixxx_to_rekordbox_xml.py
+# (Rekordbox export)
+REKBOX_OUTPUT_FILE = "rekordbox_output.xml"
+ORIGINAL_MOUNT_POINT = "/media/francois/MEGAMIX"
+FINAL_MOUNT_POINT = "E:/"
+# use a trick to calculate the "inizio" using the BPM and one of hot cues (assuming it is on a beat)
+GUESS_INIZIO = True
+# The hotcues will be converted to more robust memory cues. Do we keep the original hotcues after conversion?
+KEEP_HOT_CUES = False
+
+# parameters for ../fix_track_paths/clementine_custom_music_db.py
+# (Fixing tacks locations)
+# The names must correspond to the ones in fix_track_paths/mixxxdb_fix_tracks_locations.sql file
+# You normally do not need to change them
+CLEM_DB = "${HOME}/.config/Clementine/clementine.db"
 CUSTOM_DB = "custom_music_db.sqlite"
 CUSTOM_DB_TABLE_NAME = "library"
 CUSTOM_DB_MIXXX_IDX_COLUMN = "IDX_MIXXX_LIBRARY"
 CUSTOM_DB_PATH_COLUMN = "PATH_MUSIC_PLAYER"
 CUSTOM_DB_FILENAME_COLUMN = "FILENAME_MUSIC_PLAYER"
 CUSTOM_DB_DIRECTORY_COLUMN = "DIRECTORY_MUSIC_PLAYER"
+
+
