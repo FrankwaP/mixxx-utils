e7652f85cfc362cfc2045dca07f91cbcf724a1e0
François P <francois.plessier@gmail.com>
2024-03-29 11:00:44 +0100

    update to switch on/off

--- mixxx_prepare.sh
+++ mixxx_prepare.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
 
-
-# stopping the network
-# so far I just stop NetworkManager activities (soft solution)
-msg="Stopping the network"
-nmcli networking off 2> /dev/null && echo $msg
-
-
-# now we use the recommendantions from this page:
+# we use the recommendantions from this page:
 # https://github.com/mixxxdj/mixxx/wiki/Adjusting-Audio-Latency
 
-# theses commands needs to be called with sudo
+function network {
+	# parameter 1: on/off
+	msg="Network is $1"
+	nmcli networking "$1" > /dev/null && echo "$msg"
+}
+
+function scaling {
+	# parameter 1: schedutil/performance
+	grep -wL "$1" /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | while read -r gov; do
+		cpunum=$(echo "$gov" | grep -Po "\d+")
+		msg="Scaling for CPU #$cpunum is $1"
+		echo "$1" > "$gov" && echo "$msg"
+	done
+}
+
+function hyperthreading {
+	# parameter 1: on/off
+	ctl=/sys/devices/system/cpu/smt/control
+	msg="Hyperthreading is $1" 
+	grep -wq "$1" $ctl || { echo "$1" > "$ctl" && echo "$msg"; }
+}
+
+
+if [ $# -ne 1 ]; then
+	echo "Needs one parameter: on/off"
+    exit 1
+fi
+
+
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
-
-# CPU frequency scaling is a main cause of Mixxx skipping on laptops.
-for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
-	cpunum=$(echo $i | grep -Po "\d+")
-	msg="Disabling CPU frequency scaling for CPU #$cpunum"
-	test "$(cat $i)" = "performance" || echo $msg && echo "performance" > $i
-done
-
-# Simultaneous Multithreading (SMT), or HyperThreading (HT) as Intel calls it,
-# can make some programs faster but makes realtime audio software
-# like Mixxx much more likely to glitch.
-i=/sys/devices/system/cpu/smt/control
-msg="Disabling hyperthreading" 
-test "$(cat $i)" = "on" && echo $msg && echo "off" > $i
+case $1 in
+	on)
+		network "off"
+		scaling "performance"
+		hyperthreading "off"
+		;;
+	off)
+		network "on"
+		scaling "schedutil"
+		hyperthreading "on"
+		;;
+	*)
+		echo "Needs one parameter: on/off"
+esac
