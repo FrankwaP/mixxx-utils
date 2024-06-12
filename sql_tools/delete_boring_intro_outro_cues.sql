43d6b67364966b1d7ffa48157f9d0478bacce764
François P <francois.plessier@gmail.com>
2024-06-12 19:10:38 +0200

    sql commands to remove all the intro/outro cues

---
+++ sql_tools/delete_boring_intro_outro_cues.sql
@@ -0,0 +1,12 @@
+-- This script will delete the keys calculated with an old key detector
+-- To get the list of the keys detectors, use:
+  -- SELECT DISTINCT keys_sub_version FROM library;
+-- for example I get:
+  -- vamp_plugin_id=qm-keydetector:2
+  -- vamp_plugin_id=keyfinder:2
+-- If you want to keep only the results from "keyfinder", do:
+
+DELETE FROM cues
+  WHERE type = 6 OR type = 7;
+VACUUM;
+ANALYZE;
